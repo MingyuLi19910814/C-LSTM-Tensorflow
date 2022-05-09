4f0116304f127b9cabf0053f90d66b13eb4720bf
Mingyu Li <mingyuli19910814@gmail.com>
2022-05-09 09:49:10 +0900

    update constants

--- constants.py
+++ constants.py
@@ -5,9 +5,5 @@ EMBEDDING_DIM = 300
 SAVED_MODEL_DIR = './saved_model/'
 os.makedirs(SAVED_MODEL_DIR, exist_ok=True)
 TOKENIZER_PATH = os.path.join(SAVED_MODEL_DIR, 'tokenizer.bin')
-
-SAVED_MODEL_DIR_2 = './saved_model_2/'
-os.makedirs(SAVED_MODEL_DIR_2, exist_ok=True)
-TOKENIZER_PATH_2 = os.path.join(SAVED_MODEL_DIR_2, 'tokenizer.bin')
 SST2_DATA_DIR = './data/SST-2'
 SST5_DATA_DIR = './data/SST-5'
